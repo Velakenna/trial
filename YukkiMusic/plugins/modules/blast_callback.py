a72f32da3e7f6872232f6c9c011707d7bb4c1e2b
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-08 14:44:59 +0530

    Update blast_callback.py

--- YukkiMusic/plugins/modules/blast_callback.py
+++ YukkiMusic/plugins/modules/blast_callback.py
@@ -13,26 +13,15 @@ from YukkiMusic import app
 )
 async def on_open_me_button_click(_, etho: Union[types.Message, types.CallbackQuery]):
     print("Callback query received:", etho.message.text)
-    chat_id = etho.message.chat.id
-    time_of_day = "evening" # if "good evening" in etho.message.text.lower() # else: "morning"    
-        
-    if time_of_day == "morning":
-        print("Morning button clicked!")
-        #await etho.answer("Getting your quote...", show_alert=False)
-        await etho.edit_message_text(text="Getting your quote...")
-        await asyncio.sleep(3)
-        quote = get_random_quote()
-        await etho.edit_message_text(            
-            text=f"Good morning {etho.from_user.mention}! Here's a random quote:\n\n{quote}"
-        )
-    elif print("Evening button clicked!")
-         #await etho.answer("Getting your joke...", show_alert=False)
-         await etho.edit_message_text(text="Getting your joke...")
-         await asyncio.sleep(3)
-         joke = get_random_joke()
-         await etho.edit_message_text(
-             text=f"Good evening {etho.from_user.mention}! Here's a random joke:\n\n{joke}"
-         )
-        
+    chat_id = etho.message.chat.id    
+    #await etho.answer("Getting your quote...", show_alert=False)
+    await etho.edit_message_text(text="Getting your quote...")
+    await asyncio.sleep(3)
+    quote = get_random_quote()
+    await etho.edit_message_text(            
+        text=f"Good morning {etho.from_user.mention}! Here's a random quote:\n\n{quote}"
+    )
+    print("Morning button clicked!")    
+    
 
     await etho.answer()
