370fb232c3ccad0b135c3f8bbb06930c8f5e2375
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-08 14:47:13 +0530

    Update tag-per-on-website.py

--- YukkiMusic/plugins/modules/tag-per-on-website.py
+++ YukkiMusic/plugins/modules/tag-per-on-website.py
@@ -72,3 +72,20 @@ async def tagme_handler(client, message: Message):
     except:
         pass
 
+@app.on_callback_query(
+    filters.regex("^open_me$")
+)
+async def on_open_me_button_click(_, etho: Union[types.Message, types.CallbackQuery]):
+    print("Callback query received:", etho.message.text)
+    chat_id = etho.message.chat.id    
+    #await etho.answer("Getting your quote...", show_alert=False)
+    await etho.edit_message_text(text="Getting your quote...")
+    await asyncio.sleep(3)
+    quote = get_random_quote()
+    await etho.edit_message_text(            
+        text=f"Good morning {etho.from_user.mention}! Here's a random quote:\n\n{quote}"
+    )
+    print("Morning button clicked!")    
+    
+
+    await etho.answer()
