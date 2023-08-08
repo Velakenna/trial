24fc40c715e5c6a1335e47b77a15a90d9536f8d9
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-08 12:41:37 +0530

    Update tag-per-on-website.py

--- YukkiMusic/plugins/modules/tag-per-on-website.py
+++ YukkiMusic/plugins/modules/tag-per-on-website.py
@@ -82,14 +82,18 @@ async def on_open_me_button_click(client, etho: Union[types.Message, types.Callb
         
     if time_of_day == "morning":
         print("Morning button clicked!")
-        await etho.answer("Getting your quote...", show_alert=False)
+        #await etho.answer("Getting your quote...", show_alert=False)
+        await etho.edit_message_text(text="Getting your quote...")
+        await asyncio.sleep(3)
         quote = get_random_quote()
         await etho.edit_message_text(            
             text=f"Good morning {etho.from_user.mention}! Here's a random quote:\n\n{quote}"
         )
     else:
         print("Evening button clicked!")
-        await etho.answer("Getting your joke...", show_alert=False)
+        #await etho.answer("Getting your joke...", show_alert=False)
+        await etho.edit_message_text(text="Getting your joke...")
+        await asyncio.sleep(3)
         joke = get_random_joke()
         await etho.edit_message_text(
             text=f"Good evening {etho.from_user.mention}! Here's a random joke:\n\n{joke}"
