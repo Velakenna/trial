719198829b371bc655887ae3fcd640d62da077a6
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-08 21:08:36 +0530

    Update tag-per-on-website.py

--- YukkiMusic/plugins/modules/tag-per-on-website.py
+++ YukkiMusic/plugins/modules/tag-per-on-website.py
@@ -70,7 +70,7 @@ async def tagme_handler(client, message: Message):
         if usrnum == 1:
             markup = open_me_markup()
             tag_message = f"[{usr.user.first_name}](tg://user?id={usr.user.id}) {random.choice(TAGMES)}"
-            await message.reply_text(tag_message, reply_markup=markup)
+            await message.reply_text(tag_message, msg, reply_markup=markup)
             
             
             # Generate a random sleep time between 10 and 30 seconds
