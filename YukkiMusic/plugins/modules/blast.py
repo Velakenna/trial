6950537e03b7f516a1bc37cd912f41dddc47d57a
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-09 17:57:09 +0530

    Update blast.py

--- YukkiMusic/plugins/modules/blast.py
+++ YukkiMusic/plugins/modules/blast.py
@@ -5,13 +5,7 @@ from YukkiMusic import app
 def blast_markup():
     upl = InlineKeyboardMarkup(
         [
-            [
-                InlineKeyboardButton(
-                    text=("Blast!"),
-                    callback_data=f"blast",
-                ),
-
-            ]
+            [InlineKeyboardButton(text=("Blast!"),callback_data=f"blast")]
         ]
     )
     return upl
@@ -19,11 +13,31 @@ def blast_markup():
 def open_me_markup():
     dei = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton(text=("🌻 • ⃤•  ᴏᴘᴇɴ ᴍᴇ ! • ⃤• 🌞"),callback_data=f"open_me")],
-            [InlineKeyboardButton(text=("🥱 • ⃤•  Sυɾ-Pɾιȥҽ ! • ⃤• 🥴"),callback_data=f"surprise")],
-            [InlineKeyboardButton(text=("☕️ •̴ ⃤̴•̴  C̴l̴i̴c̴k̴-̴M̴e̴ ! •̴ ⃤̴•̴  🍔"),callback_data=f"click_me")],
-            [InlineKeyboardButton(text=("🌜• ⃤•  ｃĻ𝕠รє-Μ𝐞 ! • ⃤• 🌝"),callback_data=f"close_me")],
-            
+            [InlineKeyboardButton(text=("🌻 • ⃤•  ᴏᴘᴇɴ ᴍᴇ ! • ⃤• 🌞"),callback_data=f"open_me")]            
         ]
     )
     return dei
+
+def surprise_markup():
+    oi = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text=("🥱 • ⃤•  Sυɾ-Pɾιȥҽ ! • ⃤• 🥴"),callback_data=f"surprise")]
+        ]
+    )
+    return oii
+
+def click_me_markup():
+    hey = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text=("☕️ •̴ ⃤̴•̴  C̴l̴i̴c̴k̴-̴M̴e̴ ! •̴ ⃤̴•̴  🍔"),callback_data=f"click_me")]
+        ]
+    )
+    return hey
+
+def close_me_markup():
+    oii = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text=("🌜• ⃤•  ｃĻ𝕠รє-Μ𝐞 ! • ⃤• 🌝"),callback_data=f"close_me")]
+        ]
+    )
+    return oii
