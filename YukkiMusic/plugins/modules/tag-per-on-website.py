beac83c03557913ffdb9bc2f854d46b73cb2aab7
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-09 18:24:52 +0530

    Update tag-per-on-website.py

--- YukkiMusic/plugins/modules/tag-per-on-website.py
+++ YukkiMusic/plugins/modules/tag-per-on-website.py
@@ -119,7 +119,7 @@ async def on_open_me_button_click(client, etho: Union[types.Message, types.Callb
     if user_name in etho.message.text:
         if "good morning" in etho.message.text.lower():
             print("Morning button clicked!")
-            await etho.edit_message_text(text="Getting your quote...")
+            await etho.edit_message_text(text="Getting your joke...")
             await asyncio.sleep(2)
             joke = get_random_joke()            
             await etho.edit_message_text(text=f"Good morning {etho.from_user.mention}! Here's a random joke:\n\n{joke}")
@@ -142,33 +142,10 @@ async def on_open_me_button_click(client, etho: Union[types.Message, types.Callb
     if user_name in etho.message.text:
         if "good afternoon" in etho.message.text.lower():
             print("Afternoon button clicked!")
-            await etho.edit_message_text(text="Getting your afternoon joke...")
+            await etho.edit_message_text(text="Getting your surprise...")
             await asyncio.sleep(2)
             ta_joke = get_random_tamil_joke()
             await etho.edit_message_text(text=f"Good afternoon {etho.from_user.mention}! Here's a random Tamil joke:\n\n{ta_joke}")
-
-        else:
-            await etho.edit_message_text(text=f"Hey {etho.from_user.mention}! Sorry some error occured")
-
-        await etho.answer()
-
-    else:
-        await etho.answer("Sorry this button is not for you")
-
-@app.on_callback_query(filters.regex("close_me"))
-async def on_open_me_button_click(client, etho: Union[types.Message, types.CallbackQuery]):
-    print("Callback query received:", etho.message.text)
-    chat_id = etho.message.chat.id
-    user_name = etho.from_user.first_name
-    message_text = etho.message.text    
-    
-    if user_name in etho.message.text:
-        if "good night" in etho.message.text.lower():
-            print("Night button clicked!")
-            await etho.edit_message_text(text="Getting your night facts...")
-            await asyncio.sleep(2)            
-            quote = get_random_quote()
-            await etho.edit_message_text(text=f"Good night {etho.from_user.mention}! Here's a random quote\n\n{quote}")
 
         else:
             await etho.edit_message_text(text=f"Hey {etho.from_user.mention}! Sorry some error occured")
@@ -192,6 +169,29 @@ async def on_open_me_button_click(client, etho: Union[types.Message, types.Callb
             await asyncio.sleep(2)
             random_news = get_random_news()
             await etho.edit_message_text(text=f"Good evening {etho.from_user.mention}! Here's a random news:\n\n{random_news}")
+
+        else:
+            await etho.edit_message_text(text=f"Hey {etho.from_user.mention}! Sorry some error occured")
+
+        await etho.answer()
+
+    else:
+        await etho.answer("Sorry this button is not for you")
+
+@app.on_callback_query(filters.regex("close_me"))
+async def on_open_me_button_click(client, etho: Union[types.Message, types.CallbackQuery]):
+    print("Callback query received:", etho.message.text)
+    chat_id = etho.message.chat.id
+    user_name = etho.from_user.first_name
+    message_text = etho.message.text    
+    
+    if user_name in etho.message.text:
+        if "good night" in etho.message.text.lower():
+            print("Night button clicked!")
+            await etho.edit_message_text(text="Getting your night facts...")
+            await asyncio.sleep(2)            
+            quote = get_random_quote()
+            await etho.edit_message_text(text=f"Good night {etho.from_user.mention}! Here's a random quote\n\n{quote}")
 
         else:
             await etho.edit_message_text(text=f"Hey {etho.from_user.mention}! Sorry some error occured")
